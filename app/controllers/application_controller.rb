a964ef4f777176f3dc99fe9db6443b87300d64c9
Hayden Gomes <hayden@palominolabs.com>
2014-03-12 15:25:49 -0700

    Adding in authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  before_action :require_authentication
+
+  def require_authentication
+    unless current_user
+      flash[:error] = 'You must be logged in to access this section.'
+      redirect_to log_in_url
+    end
+  end
+
+  private
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
 end
--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,4 +1,5 @@
 class BeersController < ApplicationController
+  skip_before_action :require_authentication, only: [:index, :show]
   before_action :set_beer, only: [:show, :edit, :update, :destroy]
 
   # GET /beers
