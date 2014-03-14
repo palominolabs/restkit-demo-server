0bd803c4b8af78c71e4095f80a41a13921f14bb7
Hayden Gomes <hayden@palominolabs.com>
2014-03-14 16:19:38 -0700

    Add validation to value of review rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :rating
+  validates_numericality_of :rating, only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5
   validates_presence_of :beer
   validates_presence_of :user
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Review do
   it { should validate_presence_of :rating }
+  it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(5).only_integer }
   it { should validate_presence_of :beer }
   it { should belong_to :beer }
   it { should belong_to :user }
