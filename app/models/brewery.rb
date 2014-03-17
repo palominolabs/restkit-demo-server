2cb5ad939b286648dd3eaa4f37f83977cd53763d
Hayden Gomes <hayden@palominolabs.com>
2014-03-17 13:10:41 -0700

    Added brewery#index view and brewery average_rating method

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,4 +2,9 @@ class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
 
   validates_presence_of :name
+
+  def average_rating
+    rated_beer_scores = beers.map(&:average_rating).compact
+    (rated_beer_scores.sum / rated_beer_scores.length).round(1) if rated_beer_scores.any?
+  end
 end
--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -3,4 +3,27 @@ require 'spec_helper'
 describe Brewery do
   it { should validate_presence_of :name }
   it { should have_many(:beers).dependent :destroy }
+
+  describe '#average_rating' do
+    before do
+      @brewery = FactoryGirl.create(:brewery)
+      FactoryGirl.create(:beer, {brewery_id: @brewery.id})
+    end
+
+    context 'no rated beers' do
+      it 'should return nil' do
+        @brewery.average_rating.should be_nil
+      end
+    end
+
+    context 'has rated beers' do
+      it 'should return a rounded average' do
+        beer = FactoryGirl.create(:beer, {brewery_id: @brewery.id})
+        FactoryGirl.create(:review, {beer_id: beer.id, rating: 4})
+        FactoryGirl.create(:review, {beer_id: beer.id, rating: 5})
+        FactoryGirl.create(:review, {beer_id: beer.id, rating: 1})
+        @brewery.average_rating.should eql 3.3
+      end
+    end
+  end
 end
