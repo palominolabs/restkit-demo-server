d11e0cbf3bc6a61c5d4215cc9c1c96b5946aed95
Ace Ellett <ace@palominolabs.com>
2014-03-18 14:19:08 -0700

    Remove authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
-  before_action :require_authentication
-
-  def require_authentication
-    unless current_user
-      flash[:error] = 'You must be logged in to access this section.'
-      redirect_to log_in_url
-    end
-  end
-
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.last
   end
 end
